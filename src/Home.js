6c812af670d843abdd77d1c4e6ee5d59f6203548
Rosa Tiara <73390525+rosatiara@users.noreply.github.com>
2021-09-05 13:08:07 +0700

    update Home.js
    made blog-preview lists using useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,20 @@
+import {useState} from 'react';
+
 const Home = () => {
-  const handleClick = () => {
-    console.log("hello there!")
-  }
-
-  const handleClickAgain = (name) => {
-    console.log("well, you again," + name);
-  }
-
+  const [blogs, setBlogs] = useState([
+    {title: "My new website", body: "Lorem ipsum...", author: "mario", id: 1},
+    {title: "How to center a div", body: "Lorem ipsum...", author: "rosa", id: 2},
+    {title: "CSS is Fun", body: "Lorem ipsum...", author: "andrew", id: 3},
+  ])
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
-      <button onClick={()=>{handleClickAgain("rosa")}}>Click me again plz</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+
+        </div>
+      ))}
     </div>
    );
 }
