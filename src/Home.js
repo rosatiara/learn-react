167e07df6ed18ad310692313ef66f295f1316db0
Rosa Tiara <73390525+rosatiara@users.noreply.github.com>
2021-09-05 20:42:07 +0700

    update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,17 @@
+import {useState} from 'react';
+import BlogList from './BlogList';
+
 const Home = () => {
-  const handleClick = () => {
-    console.log("hello there!")
-  }
-
-  const handleClickAgain = (name) => {
-    console.log("well, you again," + name);
-  }
-
+  const [blogs, setBlogs] = useState([
+    {title: "My new website", body: "Lorem ipsum...", author: "mario", id: 1},
+    {title: "How to center a div", body: "Lorem ipsum...", author: "rosa", id: 2},
+    {title: "CSS is Fun", body: "Lorem ipsum...", author: "rosa", id: 3},
+  ])
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
-      <button onClick={()=>{handleClickAgain("rosa")}}>Click me again plz</button>
+      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs.filter((blog) => blog.author === "rosa")} title="Rosa's blogs" />
+      <BlogList blogs={blogs.filter((blog) => blog.author === "mario")} title="Mario's blogs" />
     </div>
    );
 }
