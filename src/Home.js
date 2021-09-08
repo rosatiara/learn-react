02df90d0ea85f45ee2737f92083f07250440d0b1
rosatiara <73390525+rosatiara@users.noreply.github.com>
2021-09-08 20:18:46 +0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,23 +3,28 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id)
     setBlogs(newBlogs)
   }
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
+    setTimeout(()=>{
+      fetch('http://localhost:8000/blogs')
       .then(res => {
         return res.json();
       })
       .then(data => {
         setBlogs(data);
+        setIsLoading(false)
       }) 
+    }, 1000)
   }, [])
 
   return (
     <div className="home">
+      {isLoading && <div>Loading...</div> }
       {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
     </div>
   );
